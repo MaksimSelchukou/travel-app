55b660da2d104fc53e84db067880228b9a1f5442
maksimselchukou <selchukouwebdev1995@gmail.com>
2022-07-25 15:30:23 +0300

    feat:Init project

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
+import 'material-icons/iconfont/material-icons.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
